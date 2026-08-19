cfa90a40a93bb37a61e414c03b3b6c79152a12aa
kblanka2k <kblank@advance2000.com>
2026-08-19 07:09:04 -0400

    remove unnecessary comment

--- src/gui/drivemappingsettings.cpp
+++ src/gui/drivemappingsettings.cpp
@@ -1,8 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
-
 #include "drivemappingsettings.h"
 
 #include "accountstate.h"
